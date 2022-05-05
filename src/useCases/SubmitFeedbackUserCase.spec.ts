252c1142819ef22530da857c8336c42d19f9b0d8
EddyPBR <edvaldojunior1310@gmail.com>
2022-05-04 23:41:14 -0300

    :white_check_mark: add unit test SubmitFeedbackUserCase

---
+++ src/useCases/SubmitFeedbackUserCase.spec.ts
@@ -0,0 +1,64 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import SubmitFeedbackUseCase from "./SubmitFeedbackUseCase";
+
+const createFeedbackSpy = jest.fn();
+const sendMailSpy = jest.fn();
+
+describe("Submit feedback", () => {
+  const submitFeedback = new SubmitFeedbackUseCase(
+    { create: createFeedbackSpy },
+    { sendMail: sendMailSpy }
+  );
+
+  it("should be able to submit a feedback", async () => {
+    await expect(
+      submitFeedback.handle({
+        type: "BUG",
+        comment: "example comment",
+        screenshot: "data:image/png;base64,test.jpg",
+      })
+    ).resolves.not.toThrow();
+
+    expect(createFeedbackSpy).toHaveBeenCalled();
+    expect(sendMailSpy).toHaveBeenCalled();
+  });
+
+  it("should not be able to submit a feedback without type", async () => {
+    await expect(
+      submitFeedback.handle({
+        type: "",
+        comment: "example comment",
+        screenshot: "data:image/png;base64,test.jpg",
+      })
+    ).rejects.toThrow();
+
+    expect(createFeedbackSpy).toHaveBeenCalled();
+    expect(sendMailSpy).toHaveBeenCalled();
+  });
+
+  it("should not be able to submit a feedback without comment", async () => {
+    await expect(
+      submitFeedback.handle({
+        type: "BUG",
+        comment: "",
+        screenshot: "data:image/png;base64,test.jpg",
+      })
+    ).rejects.toThrow();
+
+    expect(createFeedbackSpy).toHaveBeenCalled();
+    expect(sendMailSpy).toHaveBeenCalled();
+  });
+
+  it("should not be able to submit a feedback with an invalid screenshot", async () => {
+    await expect(
+      submitFeedback.handle({
+        type: "BUG",
+        comment: "ta tudo bugado!",
+        screenshot: "test.jpg",
+      })
+    ).rejects.toThrow();
+
+    expect(createFeedbackSpy).toHaveBeenCalled();
+    expect(sendMailSpy).toHaveBeenCalled();
+  });
+});
