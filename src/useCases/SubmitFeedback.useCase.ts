ab69948d08c56e3382dfc103dba40fc98cd9e834
EddyPBR <edvaldojunior1310@gmail.com>
2022-05-06 20:21:59 -0300

    :fix: SubmitFeedback.useCase imports

--- src/useCases/SubmitFeedback.useCase.ts
+++ src/useCases/SubmitFeedback.useCase.ts
@@ -1,5 +1,5 @@
-import { MailAdapter } from "~adapters/MailAdapter";
-import { FeedbacksRepository } from "~repositories/FeedbacksRepository";
+import { MailAdapter } from "~adapters/Mail.adapter";
+import { FeedbacksRepository } from "~repositories/Feedbacks.repository";
 import AppException from "~exceptions/App.exception";
 interface SubmitFeedbackUseCaseRequest {
   type: string;
@@ -40,6 +40,9 @@ class SubmitFeedbackUseCase {
         `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
         `<p>Tipo do feedback</p>`,
         `<p>Comentário: ${comment}</p>`,
+        screenshot
+          ? `<img src="${screenshot}" alt="screenshot sended by user" />`
+          : null,
         `</div>`,
       ].join("\n"),
     });
